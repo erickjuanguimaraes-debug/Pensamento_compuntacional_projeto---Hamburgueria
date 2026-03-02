00097106197899e51e6627518d7de7f62303ce08
Erick juan <erickjuanguimaraes@gmail.com>
2026-03-02 11:56:41 -0300

    uptade
    Na aula de hoje fizemos o menu de pedido da Hamburgueria com os nomes do lanches

---
+++ projeto_app_erickjuan.py
@@ -0,0 +1,72 @@
+'''
+CRUD
+
+Hamburgueria
+
+Entrega o cardapio
+Escolhe seu produto
+pagar
+retirar no caixa
+Erick Juan
+'''
+
+
+
+print(" ===HAMBURGUERIA===")
+print(".. Gestao da hamburgueria")
+print("1..Anotar o pedido")
+print("2..entrega o pedido")
+print("3fazer seu pedido")
+print("4..anotar o pagamento do cliene")
+print("5..Embalar o produto")
+print("6.entregar o pedido para o cliente")
+
+while True:
+    escolha = input("\nEscolha uma opção")
+    if escolha == '1':
+        print("gestão da hamburgeria...")
+    # codigo para entrega o cardapio
+
+
+    elif escolha =='0': 
+        print("saindo do sistema.até logo !")
+    break
+
+
+else:
+    print("opção invalida.por favor, tente novamente.")
+    
+    if escolha == '1':
+        print("Anotar pedido")
+        #codigo para notar
+
+    if escolha == '2':
+        print("Fazer seu pedido")
+        #codigo fazer seu pedido
+
+    if escolha == '3':
+        print("Anotar o pagamento do cliente")
+        # codigo anotar o pagamento do cliente
+    
+    if escolha == '4':
+        print("Embalar o produto")
+        # codigo embalar o produto
+
+    if escolha == '5':
+        print("Entrega o pedido para o cliente")
+
+        # codigo para entrega o produto para o cliente
+
+         
+
+    if escolha =='1'
+        print("Hambuguer X salada")
+        print("")
+
+
+
+
+
+
+
+        

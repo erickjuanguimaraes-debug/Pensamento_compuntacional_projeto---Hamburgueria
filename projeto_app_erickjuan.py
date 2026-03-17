10e2ab9924b154af7db28feed42845a21387a779
Erick juan <erickjuanguimaraes@gmail.com>
2026-03-16 21:35:04 -0300

    Add files via upload

--- projeto_app_erickjuan.py
+++ projeto_app_erickjuan.py
@@ -10,148 +10,93 @@ retirar no caixa
 Erick Juan
 '''
 
-
-
-
-print("..gestão da hambugueria")
-print("1.. Cadastro")
-print("2..Cardapio")
-print("3.. Preços")
-print("4..Pagamentos")
+print("=== Gestão da Hamburgueria ===")
 
 while True:
-    escolha = input("\nEscolha uma opção")
-    if escolha == '1':
-        print("Novo cadastro")
-    # codigo para entrega o cardapio
+    print("\nMenu")
+    print("1 - Cadastro")
+    print("2 - Cardápio")
+    print("3 - Preços")
+    print("4 - Pagamentos")
+    print("0 - Sair")
+
+    escolha = input("Escolha uma opção: ")
+
+    if escolha == "1":
+        print("\n--- Novo Cadastro ---")
+        nome = input("Nome: ")
+        email = input("Email: ")
+        print("Cadastro realizado com sucesso!")
+
+    elif escolha == "2":
+        print("\n--- Cardápio ---")
+
+        print("\nHambúrgueres:")
+        print("1 - X-Burger (Pão, hambúrguer bovino, queijo mussarela, alface e tomate)")
+        print("2 - X-Salada (Pão, hambúrguer bovino, queijo, alface, tomate e maionese)")
+        print("3 - X-Bacon (Pão, hambúrguer bovino, queijo, bacon crocante)")
+        print("4 - X-Egg (Pão, hambúrguer bovino, queijo e ovo frito)")
+        print("5 - X-Tudo (Pão, hambúrguer bovino, queijo, bacon, ovo, alface e tomate)")
+
+        print("\nHambúrgueres Especiais:")
+        print("6 - Smash Burger")
+        print("7 - Bacon Supreme")
+        print("8 - Cheddar Duplo")
+        print("9 - Barbecue Burger")
+
+        print("\nBatatas:")
+        print("10 - Batata Frita Pequena")
+        print("11 - Batata Frita Grande")
+        print("12 - Batata com Cheddar e Bacon")
+        print("13 - Onion Rings")
+
+        print("\nBebidas:")
+        print("14 - Refrigerante lata")
+        print("15 - Refrigerante 600ml")
+        print("16 - Água mineral")
+        print("17 - Suco natural")
+
+    elif escolha == "3":
+        print("\n--- Preços ---")
+
+        print("\nHambúrgueres:")
+        print("X-Burger - R$15")
+        print("X-Salada - R$15")
+        print("X-Bacon - R$20")
+        print("X-Egg - R$15")
+        print("X-Tudo - R$25")
+
+        print("\nHambúrgueres Especiais:")
+        print("Smash Burger - R$25")
+        print("Bacon Supreme - R$30")
+        print("Cheddar Duplo - R$30")
+        print("Barbecue Burger - R$30")
+
+        print("\nBatatas:")
+        print("Batata Frita Pequena - R$10")
+        print("Batata Frita Grande - R$18")
+        print("Batata com Cheddar e Bacon - R$22")
+        print("Onion Rings - R$22")
+
+        print("\nBebidas:")
+        print("Refrigerante lata - R$8")
+        print("Refrigerante 600ml - R$9")
+        print("Água mineral - R$5")
+        print("Suco natural - R$7")
+
+    elif escolha == "4":
+        print("\n--- Pagamentos ---")
+        print("1 - Dinheiro")
+        print("2 - Cartão")
+        print("3 - Pix")
+        pagamento = input("Escolha a forma de pagamento: ")
+        print("Pagamento registrado!")
+
+    elif escolha == "0":
+        print("Saindo do sistema... Até logo!")
+        break
+
+    else:
+        print("Opção inválida. Tente novamente.")
 
 
-    elif escolha =='0': 
-        print("saindo do sistema.até logo !")
-    break
-
-
-else:
-
-    
-    if escolha == '1':
-        print("Fazer seu cadastro")
-        #codigo para entrega o pedido
-
-    elif escolha == '2':
-        print("Cardapio")
-        #codigo fazer seu pedido
-
-    elif escolha == '3':
-        print("Preços")
-        # codigo anotar o pagamento do cliente
-    
-    elif escolha == '4':
-        print("Pagamentos")
-        # codigo embalar o produto
-    
-         
-
-    if escolha == '1' :
-        print("\n--- Novo Cadastro---")
-        
-
-    #dados solicitados ao cliente
-    nome = input('nome :')
-    email = input('email :')
-
-
-    if escolha == '2':
-        print("\n--- Cardapio---")
-        input('escolha seu pedido')
-
-        print("-BurgerX Pão, hambúrguer bovino , queijo mussarela, alface e tomate.")
-        print("X-Salada Pão, hambúrguer bovino , queijo, alface, tomate e maionese da casa.")
-        print("X-Bacon Pão, hambúrguer bovino , queijo, bacon crocante e maionese da casa.")
-        print("X-Egg Pão, hambúrguer bovino , queijo e ovo frito.")
-        print("X-Tudo Pão,hambúrguer bovino, queijo, bacon, ovo, alface, tomate, milho, batata palha e maionese.")
-        print("Smash Burger Pão brioche, 2 smash burgers 80g, queijo cheddar e molho especial.")
-        print("Bacon Supreme ão brioche, hambúrguer 150g, cheddar, bacon crocante e cebola caramelizada.")
-        print("Cheddar Duplo ão brioche, 2 hambúrgueres, cheddar cremoso e molho especial.")
-        print("Barbecue Burger Pão brioche, hambúrguer , queijo, bacon e molho barbecue.")
-
-    print("Batata Frita Pequena")
-
-    print("Batata Frita Grande")
-
-    print("Batata com Cheddar e Bacon")
-
-    print("Onion Rings (Anéis de cebola")
-
-    print("Bebidas")
-
-    print("Refrigerante em lata")
-
-    print("Refrigerante 600ml")
-    
-    print("Aguá mineral")
-
-    print("Suco natural")
-
-    if escolha == '3':
-        
-        print("\n---Preços---")
-
-        print(" X Burger")
-        print("15$")
-
-        print(" X salada")
-        print("15$")
-
-        print("X-Bacon")
-        print("20$")
-
-        print("X-Egg")
-        print("15$")
-
-        print("X- tudo")
-        print("25$")
-
-    print("\n---Hambugueres especias---")
-
-    print("Smash Burger")
-    print("25$")
-
-    print("Bacon Supreme ")
-    print("30$")
-
-    print("Cheddar Duplo")
-    print("30$")
-
-    print("Barbecue Burger ")
-    print("30$")
-
-    print("\n---Batatas---")
-
-    print("Batata Frita Pequena")
-    print("10$")
-
-    print("Batata Frita Grande")
-    print("18$")
-
-    print("Batata com Cheddar e Bacon")
-    print("22$")
-
-    print("Onion Rings (Anéis de cebola")
-    print("22$")
-
-    print("\n---Bebidas---")
-
-    print("Refrigerante em lata")
-    print("8$")
-
-    print("Refrigerante 600ml")
-    print("9$")
-    
-    print("Aguá mineral")
-    print("5$")
-
-    print("Suco natural")
-    print("7$")
-
-    

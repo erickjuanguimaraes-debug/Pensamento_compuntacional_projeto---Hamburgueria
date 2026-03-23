42a7a9d843bcdda11a5c564a5d4ef0c406f0d98c
Erick juan <erickjuanguimaraes@gmail.com>
2026-03-23 10:45:03 -0300

    Add files via upload
    upgrade

--- projeto_app_erickjuan.py
+++ projeto_app_erickjuan.py
@@ -18,20 +18,24 @@ while True:
     print("2 - Cardápio")
     print("3 - Preços")
     print("4 - Pagamentos")
+    print("5 - Entrega")
+    print("6.feedback")
     print("0 - Sair")
 
     escolha = input("Escolha uma opção: ")
 
     if escolha == "1":
-        print("\n--- Novo Cadastro ---")
+        print("\nNovo Cadastro")
+
         nome = input("Nome: ")
         email = input("Email: ")
         print("Cadastro realizado com sucesso!")
 
     elif escolha == "2":
-        print("\n--- Cardápio ---")
+        print("\nCardápio")
 
         print("\nHambúrgueres:")
+
         print("1 - X-Burger (Pão, hambúrguer bovino, queijo mussarela, alface e tomate)")
         print("2 - X-Salada (Pão, hambúrguer bovino, queijo, alface, tomate e maionese)")
         print("3 - X-Bacon (Pão, hambúrguer bovino, queijo, bacon crocante)")
@@ -57,7 +61,7 @@ while True:
         print("17 - Suco natural")
 
     elif escolha == "3":
-        print("\n--- Preços ---")
+        print("\nPreços")
 
         print("\nHambúrgueres:")
         print("X-Burger - R$15")
@@ -85,18 +89,41 @@ while True:
         print("Suco natural - R$7")
 
     elif escolha == "4":
-        print("\n--- Pagamentos ---")
+        print("\nPagamentos")
+
         print("1 - Dinheiro")
         print("2 - Cartão")
         print("3 - Pix")
         pagamento = input("Escolha a forma de pagamento: ")
         print("Pagamento registrado!")
 
+    elif escolha == "5" :
+
+        print("entrega")
+        print("\n Entrega :")
+        input("Confirmar sua entrega com sim ou não :")
+        print('Seu pedido saiu para entrega ')
+        print('seu pedido saiu para entregar')
+        input('seu pedido foi entregue com sucesso')
+
+
+    elif escolha == "6":
+        print('mande sua gestação')
+        print('manda seu feedback')
+
+        input('mande seu feedback\sua sugestão para nossa loja')
+        print('obrigado pela sua sugestão\feedback')
+
+
+
+
+
+
+
+
+
+
+
     elif escolha == "0":
-        print("Saindo do sistema... Até logo!")
+        print("Saindo do sistema.obrigado por visitar nossa hambugueria.")
         break
-
-    else:
-        print("Opção inválida. Tente novamente.")
-
-
